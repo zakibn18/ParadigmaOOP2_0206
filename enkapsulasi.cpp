e45c3cb657dc3945f7b24ea24f5f039af301c819
zakibn18 <jekben815@gmail.com>
2025-06-02 18:35:41 +0700

    membuat metode

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -5,6 +5,16 @@ class remoteLampu
 {
 private:
   string saklarNo[10];
+
+public:
+  void setSaklarNo(int i, string value)
+  {
+    saklarNo[i] = value;
+  }
+  string getSaklarNo(int i)
+  {
+    return saklarNo[i];
+  }
 };
 
 int main()
