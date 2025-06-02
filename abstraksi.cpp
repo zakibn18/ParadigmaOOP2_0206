fd73dd7c2eede0414b88bc832e6654a92702423c
zakibn18 <jekben815@gmail.com>
2025-06-02 18:16:44 +0700

    mengisi nilai private member

--- abstraksi.cpp
+++ abstraksi.cpp
@@ -5,6 +5,15 @@ class AbstraksiKlas
 {
 private:
   string x, y;
+
+public:
+  //metode untuk mengisi nilai
+  //private member
+  void setXY(string a, string b)
+  {
+    x = a;
+    y = b;
+  }
 };
 
 int main()
