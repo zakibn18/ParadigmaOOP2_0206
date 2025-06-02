8d04c3bb6d3b3dac5a68369785a162ce6d02d109
zakibn18 <jekben815@gmail.com>
2025-06-02 18:46:45 +0700

    membuat class baseClass dengan final

--- noPewarisan.cpp
+++ noPewarisan.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 using namespace std;
 
+class baseClass final
+{
+public:
+  virtual void perkenalan()
+  {
+    cout << "Hallo saya fungtion dari base Class";
+  }
+};
+
 int main()
 {
-  
+
 }
